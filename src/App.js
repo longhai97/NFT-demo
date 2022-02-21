2dd1f58f0178a2022858083021bc4c33428a418a
tpazyot127 <62755166+tpazyot127@users.noreply.github.com>
2022-02-21 09:57:09 +0700

    auto add token

--- src/App.js
+++ src/App.js
@@ -34,6 +34,12 @@ function App() {
         { img: 'https://wallpapercave.com/wp/wp8806278.jpg' },
     ]
 
+
+    const tokenAddress = '0xd00981105e61274c8a5cd5a88fe7e037d935b513';
+    const tokenSymbol = 'TUT';
+    const tokenDecimals = 18;
+    const tokenImage = 'http://placekitten.com/200/300';
+
     useLayoutEffect(() => {
         const fetchInitialData = async () => {
             // Crete Provider
@@ -50,6 +56,33 @@ function App() {
             console.log('balance', balance);
         }
         fetchInitialData()
+        const autoAddToken = async () => {
+            const { ethereum } = window;
+            try {
+                // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+                const wasAdded = await ethereum.request({
+                    method: 'wallet_watchAsset',
+                    params: {
+                        type: 'ERC20', // Initially only supports ERC20, but eventually more!
+                        options: {
+                            address: tokenAddress, // The address that the token is at.
+                            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                            decimals: tokenDecimals, // The number of decimals in the token
+                            image: tokenImage, // A string url of the token logo
+                        },
+                    },
+                });
+
+                if (wasAdded) {
+                    console.log('Thanks for your interest!');
+                } else {
+                    console.log('Your loss!');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        autoAddToken()
     }, [balance]);
 
     const checkWalletIsConnected = async () => {
